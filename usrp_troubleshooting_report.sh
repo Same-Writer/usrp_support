c588cd5cdd838eeaf6ca6093b69dd66b4bfe7129
Same-Writer <sam.reiter@ni.com>
2019-12-10 10:38:52 -0600

    update to include nate's recommendations for extra info + terminal output for transparency

--- usrp_troubleshooting_report.sh
+++ usrp_troubleshooting_report.sh
@@ -4,27 +4,42 @@
 #.txt files, and then compress. Send this to the support team.
 DATE=$(date +'%m_%d_%Y')
 WORKING_DIR=""./usrp_report_"$DATE"
+PREFIX_RETURN="$(uhd_config_info --install-prefix)"
+UHD_INSTALL_DIR=${PREFIX_RETURN#"Install prefix: "}
 
 get_host_sw_cmds=(
 "uname -a"
+"cat /etc/lsb-release"
 #"comment anything you want to omit like this"
-"ifconfig"
+"ip a"
 "cpufreq-info"
+"cat /proc/cpuinfo"
+"dpkg -l"
 )
 
 get_host_hw_cmds=(
 "uhd_find_devices"
 #"uhd_usrp_probe --args "type=b200""
 "lshw"
+"lsusb"
+"lspci"
+"lspci -vvv"
+"lspci -t"
 )
 
 get_uhd_cmds=(
 "uhd_usrp_probe"
-#"uhd_usrp_probe --args "addr=123.123.123.123,serial=1234567""
+#"uhd_usrp_probe --args "addr=123.123.123.123,serial=1234567"
+"uhd_config_info --print-all"
+"$UHD_INSTALL_DIR/lib/uhd/utils/usrp_burn_mb_eeprom --read-all"
+"$UHD_INSTALL_DIR/lib/uhd/utils/usrp_burn_db_eeprom"
+"$UHD_INSTALL_DIR/lib/uhd/utils/b2xx_fx3_utils"
 )
 
 get_gr_cmds=(
 "gnuradio-companion --version"
+"gnuradio-config-info --enabled-components"
+"gnuradio-config-info --prefix"
 )
 
 file_names=(
@@ -33,6 +48,36 @@ file_names=(
 "get_uhd_cmds"
 "get_gr_cmds"
 )
+
+user_message(){
+echo -e "
+*****************************************************************************
+* This utility will record several hardware and software details about your
+* system. The outputs will be logged to .txt and .html files. Those files
+* will get compressed as *.tar.gz so you can email them to technical support.
+* Here's everything we're planning to log on this run:
+*"
+
+printf '\t%s\n' "${get_host_sw_cmds[@]}"
+printf '\t%s\n' "${get_host_hw_cmds[@]}"
+printf "\tlshw -html (comment last line of 'get_host_hw_info' to omit)\n"
+printf '\t%s\n' "${get_gr_cmds[@]}"
+printf '\t%s\n' "${get_uhd_cmds[@]}"
+
+echo -e "\nWould you like to continue? (y/n)"
+read user_decision
+
+	if [ "$user_decision" = "y" ]
+	then
+		:
+	elif [ "$user_decision" = "n" ];
+		then
+		exit 1
+	else
+		echo "Invalid Input, try again!"
+		user_message
+	fi
+}
 
 make_dir(){
 	mkdir -p $WORKING_DIR
@@ -56,6 +101,8 @@ get_host_hw_info(){
 	echo -e '\n~~~'${get_host_hw_cmds[i]}'~~~~\n' 	>>$WORKING_DIR/${file_names[1]}.txt
 	${get_host_hw_cmds[i]} 													&>>$WORKING_DIR/${file_names[1]}.txt
 	done
+
+	lshw -html &>>$WORKING_DIR/hardware.html
 }
 
 get_uhd_info(){
@@ -82,6 +129,7 @@ compress_dir(){
 	tar --warning=no-file-changed -czf $WORKING_DIR/$WORKING_DIR".tar.gz" $WORKING_DIR
 }
 
+user_message
 make_dir
 get_host_sw_info
 get_host_hw_info
